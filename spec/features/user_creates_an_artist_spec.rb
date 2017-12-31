3fcb575bf73e39673fb04031959e1fb052b15b10
katyjane8 <katy.jane8@gmail.com>
2017-12-31 13:36:23 -0700

    working with artists and images

---
+++ spec/features/user_creates_an_artist_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "User submits a new artist" do
+  scenario "they enter data to create a new artist" do
+    artist_name = "Adele"
+    artist_image_path = "https://www.billboard.com/files/styles/article_main_image/public/media/Adele-Press-photo-2015-billboard-Columbia-1548.jpg"
+
+    visit '/artists'
+    click_on "New Artist"
+    fill_in "artist_name", with: artist_name
+    fill_in "artist_image_path", with: artist_image_path
+    click_on "Create Artist"
+
+    expect(page).to have_content artist_name
+    expect(page).to have_css("img[src=\"#{artist_image_path}\"]")
+  end
+
+  context "the submitted data is invalid" do
+    scenario "they see an error message" do
+      pending
+      artist_image_path = "https://www.billboard.com/files/styles/article_main_image/public/media/Adele-Press-photo-2015-billboard-Columbia-1548.jpg"
+
+      visit artists_path
+      click_on "New Artist"
+      fill_in "artist_image_path", with: artist_image_path
+      click_on "Create Artist"
+
+      expect(page).to have_content "Name can't be blank"
+    end
+  end
+end
